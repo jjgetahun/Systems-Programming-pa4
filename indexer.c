4b10e1e0e5562ff9db155b6d691f47b08f33a09b
Jonathan Getahun <jjg224@cd.cs.rutgers.edu>
2015-10-29 18:35:15 -0400

    Added stuff. - JGet

--- indexer.c
+++ indexer.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <dirent.h>
+#include <sys/stat.h>
 #include <errno.h>
 #include "tokenizer.h"
 
@@ -25,23 +26,31 @@ int main (int argc, char ** argv) {
     DIR * dir;
     dirent entry;
     extern int errno;
+struct stat sb;
 
-    if (argc != 2) {
+    if (argc != 2) { /*If there are not two arguments*/
         fprintf(stderr, "You must specify a single file or directory name on the command line.\n");
         return 1;
     }
-    if ((file = fopen(argv[1], "r")) == NULL && (dir = opendir(argv[1])) == 0) {
-    	fprintf(stderr, "Could not open %s as file or directory: %s.\n", argv[1], strerror(errno));
+else {
+stat(argv[1], &sb);
+    if (/*(file = fopen(argv[1], "r")) == NULL && (dir = opendir(argv[1])) == 0*/!S_ISREG(sb.st_mode) && !S_ISDIR(sb.st_mode)) { /*If the argument is not a file nor a directory*/
+    	fprintf(stderr, "Could not open \"%s\" as a file or directory.\n", argv[1]);
         return 1;
     }
     else {
-    	if ((file = fopen(argv[1], "r")) != NULL) {
+    	if (/*(file = fopen(argv[1], "r")) != NULL*/S_ISREG(sb.st_mode)) { /*If the argument is a file*/
+printf("%d\n", errno);
+file = fopen(argv[1], "r");
             tokenize(file);
+	    fclose(file);
         }
-        if ((dir = opendir(argv[1])) != 0) {
+        if (/*(dir = opendir(argv[1])) != 0*/S_ISDIR(sb.st_mode)) { /*If the argument is a directory*/
+dir = opendir(argv[1]);
             findDirs(dir, entry);
             closedir(dir);
         }
     }
+}
     return 0;
 }
