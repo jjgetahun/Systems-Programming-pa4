33a4cea313138b98c6e8bb3119fda3b700fa8846
Jon Getahun <Jon@nbp-135-90.nbp.ruw.rutgers.edu>
2015-10-29 18:45:56 -0400

    Added stuff. - JGet

--- indexer.c
+++ indexer.c
@@ -26,31 +26,30 @@ int main (int argc, char ** argv) {
     DIR * dir;
     dirent entry;
     extern int errno;
-struct stat sb;
+    struct stat sb;
 
     if (argc != 2) { /*If there are not two arguments*/
         fprintf(stderr, "You must specify a single file or directory name on the command line.\n");
         return 1;
     }
-else {
-stat(argv[1], &sb);
-    if (/*(file = fopen(argv[1], "r")) == NULL && (dir = opendir(argv[1])) == 0*/!S_ISREG(sb.st_mode) && !S_ISDIR(sb.st_mode)) { /*If the argument is not a file nor a directory*/
-    	fprintf(stderr, "Could not open \"%s\" as a file or directory.\n", argv[1]);
-        return 1;
-    }
-    else {
-    	if (/*(file = fopen(argv[1], "r")) != NULL*/S_ISREG(sb.st_mode)) { /*If the argument is a file*/
-printf("%d\n", errno);
-file = fopen(argv[1], "r");
-            tokenize(file);
-	    fclose(file);
+    else { /*If there are two arguments*/
+        stat(argv[1], &sb);
+        if (!S_ISREG(sb.st_mode) && !S_ISDIR(sb.st_mode)) { /*If the argument is not a file nor a directory*/
+            fprintf(stderr, "Could not open \"%s\" as a file or directory.\n", argv[1]);
+            return 1;
         }
-        if (/*(dir = opendir(argv[1])) != 0*/S_ISDIR(sb.st_mode)) { /*If the argument is a directory*/
-dir = opendir(argv[1]);
-            findDirs(dir, entry);
-            closedir(dir);
+        else {
+            if (S_ISREG(sb.st_mode)) { /*If the argument is a file*/
+                file = fopen(argv[1], "r");
+                tokenize(file);
+                fclose(file);
+            }
+            if (S_ISDIR(sb.st_mode)) { /*If the argument is a directory*/
+                dir = opendir(argv[1]);
+                findDirs(dir, entry);
+                closedir(dir);
+            }
         }
     }
-}
     return 0;
 }
