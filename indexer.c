051bd605507d125b140c8de3c0808089458cdf76
Jonathan Getahun <jjg224@cd.cs.rutgers.edu>
2015-10-29 16:58:12 -0400

    Added stuff. - JGet

--- indexer.c
+++ indexer.c
@@ -6,34 +6,38 @@
 #include <errno.h>
 #include "tokenizer.h"
 
-int main (int argc, char ** argv) {
+typedef struct dirent * dirent;
 
+void findDirs(DIR * dir, dirent entry) {
+
+    while ((entry = readdir(dir)) != 0) {
+	if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+	    continue;
+	}
+	printf("d_ino %d, d_off %d, d_reclen %d, d_name %s\n", entry->d_ino, entry->d_off, entry->d_reclen, entry->d_name);
+	printf("%s\n", entry->d_name);
+    }
+
+}
+
+int main (int argc, char ** argv) {
+    
     DIR * dir;
-    //typedef struct dirent * dire;
-    //dire entry;
-    struct dirent * entry;
+    dirent entry;
     extern int errno;
 
     if (argc < 2) {
         fprintf(stderr, "Must specify directory name on command line.\n");
         return 1;
     }
-    dir = opendir(argv[1]);
-    if (dir == NULL) {
+    else if ((dir = opendir(argv[1])) == 0) {
         fprintf(stderr, "Could not open %s as directory: %s.\n", argv[1], strerror(errno));
         return 1;
     }
     else {
-        printf("Begin directory info list.\n");
-        entry = readdir(dir);
-        while (entry != NULL) {
-           // if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-	   // continue;
-	   // }
-	    printf("d_ino %d, d_off %d, d_reclen %d, d_name %s.\n", entry->d_ino, entry->d_off, entry->d_reclen, entry->d_name);
-            printf("%s.\n", entry->d_name);
-        }
-        closedir(dir);
+	printf("Begin directory info list.\n");
+	findDirs(dir, entry);
+	closedir(dir);
     }
     return 0;
 }
